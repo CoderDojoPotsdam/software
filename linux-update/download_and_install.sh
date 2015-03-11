20228f5e6c3712bc8e20b0020f7256fbb128a7b7
Coder Dojo Potsdam <niccokunzmann@aol.com>
2015-03-11 08:49:20 +0100

    renamed file

--- linux-update/download_and_install.sh
+++ linux-update/download_and_install.sh
@@ -1,2 +1,1 @@
-
 ( cd /tmp ; sudo apt-get install git ; rm -rf software ; git clone https://github.com/CoderDojoPotsdam/software.git --depth 1 ; cd software/linux-update && sudo ./install_update-service.sh )
