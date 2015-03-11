a834c25bfb95f66a3cb5a2bf18c70e988d439b0a
Coder Dojo Potsdam - Himbeere <coderdojopotsdam-discuss@googlegroups.com>
2015-03-11 15:13:21 +0100

    removed bug in waiting for internet connection

--- linux-update/wait_for_internet_connection.sh
+++ linux-update/wait_for_internet_connection.sh
@@ -6,7 +6,7 @@
 
 routesToTheInternet=0
 
-while [ $connections -e 0 ]
+while [ $routesToTheInternet -e 0 ]
 do
   # wait some time
   # http://www.unix.com/shell-programming-and-scripting/42396-wait-5-seconds-shell-script.html
@@ -15,5 +15,4 @@ do
   # check for an internet connection
   # http://stackoverflow.com/questions/1406644/checking-internet-connection-with-command-line-php-on-linux
   routesToTheInternet=`/sbin/route -n | grep -c '^0\.0\.0\.0'`
-
 done
