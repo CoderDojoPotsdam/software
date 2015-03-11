01be70b09280210860f200d867d32315c6978d53
Coder Dojo Potsdam <niccokunzmann@aol.com>
2015-03-11 11:30:41 +0100

    updated reame

--- linux-update/README.md
+++ linux-update/README.md
@@ -1,6 +1,9 @@
 Auto-update functionality for Ubuntu
 ====================================
 
+Installation
+------------
 
+Execute the file [download_and_install.sh](download_and_install.sh)
 
 
--- linux-update/download_and_install.sh
+++ linux-update/download_and_install.sh
@@ -1,1 +1,1 @@
-( cd /tmp ; sudo apt-get install git ; rm -rf software ; git clone https://github.com/CoderDojoPotsdam/software.git --depth 1 ; cd software/linux-update && sudo ./install_update-service.sh )
+( cd /tmp ; sudo apt-get install git ; rm -rf software ; git clone https://github.com/CoderDojoPotsdam/software.git --depth 1 ; cd software/linux-update && ./install_update-service.sh )
