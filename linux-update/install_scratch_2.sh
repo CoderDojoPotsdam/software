f24c64264e3b8455fc8a66e32a6ebf645e25a9b5
Coder Dojo Potsdam - ubuntu <coderdojopotsdam-discuss@googlegroups.com>
2015-05-15 09:47:28 -0700

    added scratch installer

---
+++ linux-update/install_scratch_2.sh
@@ -0,0 +1,34 @@
+#/bin/sh
+
+echo run with sudo 
+
+# http://askubuntu.com/a/440540
+apt-get -y install libxt6:i386 libnspr4-0d:i386 libgtk2.0-0:i386 libstdc++6:i386 libnss3-1d:i386 libnss-mdns libxml2:i386 libxslt1.1:i386 libcanberra-gtk-module:i386 gtk2-engines-murrine:i386
+apt-get -y install libgnome-keyring0:i386
+
+cd /tmp
+
+# download adobe air
+if [ ! -f adobeair_2.6.0.19170-devolo1_i386.deb ]
+then
+  wget http://update.devolo.com/linux/apt/pool/main/a/adobeair/adobeair_2.6.0.19170-devolo1_i386.deb
+fi
+
+# install adobe air
+# http://askubuntu.com/a/441067
+# http://askubuntu.com/questions/40779/how-do-i-install-a-deb-file-via-the-command-line
+sudo dpkg -i adobeair_2.6.0.19170-devolo1_i386.deb 
+
+# http://stackoverflow.com/questions/19261098/how-to-run-execute-an-adobe-air-file-on-linux-ec2-ubuntu-from-command-line-onl
+rm /usr/sbin/airinstall
+ln -s "/opt/Adobe AIR/Versions/1.0/Adobe AIR Application Installer" /usr/sbin/airinstall
+
+# download Scratch 2
+if [ ! -f Scratch-*.air ]
+then 
+  wget https://scratch.mit.edu/scratchr2/static/sa/Scratch-436.air
+fi
+
+airinstall `pwd`/Scratch-*.air
+
+
