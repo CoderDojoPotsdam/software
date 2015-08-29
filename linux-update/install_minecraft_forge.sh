418c0bf148d854194f94e6972b7f835064c7b2cf
Coder Dojo Potsdam - Himbeere <coderdojopotsdam-discuss@googlegroups.com>
2015-08-29 11:32:21 +0200

    changed minecraft forge

--- linux-update/install_minecraft_forge.sh
+++ linux-update/install_minecraft_forge.sh
@@ -4,7 +4,7 @@ source ./coder-dojo-profile-script.sh
 
 # set the variables we need
 UPDATE_USER_DESKTOP_PATH=`su $UPDATE_USERNAME -c 'echo $(xdg-user-dir DESKTOP)'`
-FORGE_DIR=$UPDATE_USER_DESKTOP_PATH/minecraft-forge
+FORGE_DIR=$UPDATE_USER_DESKTOP_PATH/minecraft-forge-1.7.x
 TEMP_FORGE_DIR=/tmp/forge
 FORGE_ZIP=/tmp/mincraft-forge.zip
 FORGE_URL=http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.7.10-10.13.4.1448-1.7.10/forge-1.7.10-10.13.4.1448-1.7.10-src.zip
@@ -34,7 +34,8 @@ unzip -o -qq $FORGE_ZIP -d $TEMP_FORGE_DIR
 ( cd $TEMP_FORGE_DIR ; ./gradlew setupDecompWorkspace --refresh-dependencies ; ) || { echo "ERROR: 1 gradle error $?" ; exit 3 ; }
 
 
-( cd $TEMP_FORGE_DIR ; ./gradlew eclipse ; ) || { echo "ERROR: 2 gradle error $?" ; exit 4 ; }
+( cd $TEMP_FORGE_DIR ; ./gradlew idea ; ) || { echo "ERROR: 2.1 gradle error $?" ; exit 4 ; }
+( cd $TEMP_FORGE_DIR ; ./gradlew eclipse ; ) || { echo "ERROR: 2.2 gradle error $?" ; exit 4 ; }
 
 rm -rf $FORGE_DIR
 echo deleted $FORGE_DIR, moving $TEMP_FORGE_DIR to $FORGE_DIR
