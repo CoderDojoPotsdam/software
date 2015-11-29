3253184170555b25f98ae94207a717914cc3257c
Coder Dojo Potsdam - ubuntu <coderdojopotsdam-discuss@googlegroups.com>
2015-11-29 14:43:46 -0800

    started impementing installation of the hamstermodell

---
+++ linux-update/hamstersimulator.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# start the hamstersimulator
+
+log_file=/tmp/hamstersimulator.log
+
+for java_version in `ls /opt/hamstermodell`
+do
+  if java -version 2>&1 | grep -q "java version \"$java_version"
+  then
+    # found the folder with the java version
+    jar_file=`find /opt | grep 'hamstersimulator.jar' | head -n1`
+    if [ "$jar_file" == "" ]
+    then
+      rm -f "/opt/hamstermodell/$java_version/VERSION"
+      notify-send "Could not find hamstersimulator.jar for java version $java_version"
+    fi
+    # start the simulator and measure time
+    cd "`dirname $jar_file`"
+
+    # fill the log file    
+    echo ----------------------------------- >> $log_file
+    echo java_version: $java_version
+    java -version 2>> $log_file 1>> $log_file
+
+    # measure time from http://stackoverflow.com/a/22517555
+    start_time=$(date -u +"%s")
+    java -jar "$jar_file" 2>>$log_file1>>$log_file
+    end_time=$(date -u +"%s")
+    # compare from http://stackoverflow.com/a/18668580
+    if (( start_time + 10 < end_time ))
+    then
+      notify-send "Could not start the Hamstersimulator. See $log_file for more information or run java -jar \"$jar_file\"."
+    fi
+  fi
+done
---
+++ linux-update/install_hamstermodell.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This installs the Hamstermodell from the Website
+#    http://www.java-hamster-modell.de/simulator.html#download
+
+# using arrays
+#    http://www.linuxjournal.com/content/bash-arrays
+package_urls=('http://www.java-hamster-modell.de/download/v29/hamstersimulator-v29-03-java8.zip' 
+              'http://www.java-hamster-modell.de/download/v29/hamstersimulator-v29-01-java7.zip')
+java_versions=('1.7'
+               '1.8')
+destination_path='/opt/hamstermodell'
+
+mkdir -p $destination_path
+
+# check dependencies
+if ! type unzip 1>/dev/null 2>/dev/null
+then
+  echo 'Command "unzip" not found, exiting.'
+fi
+
+for i in ${!java_versions[*]}
+do
+  java_version=${java_versions[$i]}
+  url=${package_urls[$i]}
+  path=$destination_path/$java_version
+  version_path=$path/VERSION
+  version=$url
+  download_file="/tmp/hamstersimulator${java_version}.zip"
+  if [! -f $version_path ] || [ "`cat $version_path 2>/dev/null`" != "$version" ]
+  then
+    # version file is different or not existent -> create the file
+    rm -rf "$path"
+
+    mkdir -p $path
+
+    echo download the file $url
+    wget -q -O "$download_file" "$url" || { echo "Error: could not download ${url}."; continue; }
+
+    echo unpack the archive "$download_file" to "$path"
+    unzip -q "$download_file" -d "$path"
+
+    # this is the last step to verify we did it correctly    
+    echo "$version" > "$version_path"
+  fi
+done
+
+# prepare icons
+if [ ! -f "$destination_path"/hamstersimulator.gif ]
+then
+  wget -q -O "$destination_path"/hamstersimulator.gif 'http://www.java-hamster-modell.de/bilder/bild-home.gif'
+fi
+
+# create executable and icons
+cp -u "`dirname $0`/hamstersimulator.sh" "$destination_path"
+
+cp -u "`dirname $0`/hamstersimulator.desktop" /usr/share/applications/
+cp -u "`dirname $0`/hamstertutorial.desktop" /usr/share/applications/
+
+
