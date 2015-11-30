6696d9619d2f9eef20b337a64856cf267bebf85a
Coder Dojo Potsdam - ubuntu <coderdojopotsdam-discuss@googlegroups.com>
2015-11-29 16:00:42 -0800

    changed startup homepage of firefox

--- linux-update/set_startup_homepage.sh
+++ linux-update/set_startup_homepage.sh
@@ -2,5 +2,28 @@
 
 # firefox
 # http://askubuntu.com/questions/73474/how-to-install-firefox-addon-from-command-line-in-scripts
-echo "user_pref(\"browser.startup.homepage\", \"$1\");" >> $UPDATE_HOME/.mozilla/firefox/*.default/prefs.js
+for user_name in `ls /home/`
+do
+  preferences_file="`echo /home/$user_name/.mozilla/firefox/*.default/prefs.js`"
+  if [ -f "$preferences_file" ]
+  then
+    echo "user_pref(\"browser.startup.homepage\", \"https://coderdojopotsdam.github.io/\");" >> $preferences_file
+  fi
+done
 
+# opera
+python3 -c 'import json
+import os
+for user_name in os.listdir("/home/"):
+  preferences_file = "/home/{}/.config/opera/Preferences".format(user_name)
+  if os.path.isfile(preferences_file):
+    with open(preferences_file) as file:
+      preferences = json.load(file)
+    preferences["session"] = {
+        "restore_on_startup":4,
+        "startup_urls":["https://coderdojopotsdam.github.io/"],
+        "urls_signature":"XunzeXhkToGKRY1+Zf98KY0skksrNYV2vciHYl4IKwI2De5OKDgrUU1Z5Q4UB8vo"
+      }
+    with open(preferences_file, "w") as file:
+      json.dump(preferences, file)
+'
